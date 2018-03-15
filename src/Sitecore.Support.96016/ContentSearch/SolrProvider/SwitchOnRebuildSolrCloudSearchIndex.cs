fa1076966061048c1e3aea630f8416159c263985
valeriyleman <vle@sitecore.net>
2018-03-15 17:08:19 +0200

    Initial source code import

---
+++ src/Sitecore.Support.96016/ContentSearch/SolrProvider/SwitchOnRebuildSolrCloudSearchIndex.cs
@@ -0,0 +1,98 @@
+﻿namespace Sitecore.Support.ContentSearch.SolrProvider
+{
+    using Sitecore.ContentSearch;
+    using Sitecore.ContentSearch.Diagnostics;
+    using Sitecore.ContentSearch.Maintenance;
+    using Sitecore.ContentSearch.SolrProvider;
+    using Sitecore.ContentSearch.SolrProvider.SolrOperations;
+    using Sitecore.ContentSearch.Utilities;
+    using StringExtensions;
+    using System.Threading;
+
+    public class SwitchOnRebuildSolrCloudSearchIndex : Sitecore.ContentSearch.SolrProvider.SwitchOnRebuildSolrCloudSearchIndex
+    {
+        public SwitchOnRebuildSolrCloudSearchIndex(string name, string mainalias, string rebuildalias, string activecollection, string rebuildcollection, ISolrOperationsFactory solrOperationsFactory, IIndexPropertyStore propertyStore) : base(name, mainalias, rebuildalias, activecollection, rebuildcollection, solrOperationsFactory, propertyStore)
+        {
+        }
+
+        protected override void PerformRebuild(bool resetIndex, bool optimizeOnComplete, IndexingOptions indexingOptions, CancellationToken cancellationToken)
+        {
+            if (!this.ShouldStartIndexing(indexingOptions))
+            {
+                return;
+            }
+
+            using (new RebuildIndexingTimer(this.PropertyStore))
+            {
+                // Clear collection before populating it.
+
+                if (resetIndex)
+                {
+                    this.rebuildSolrOperations = this.solrOperationsFactory.GetSolrOperations(this.RebuildCore);
+                    this.Reset();
+                }
+
+                using (IProviderUpdateContext context = this.CreateTemporaryCoreUpdateContext(this.rebuildSolrOperations))
+                {
+                    foreach (IProviderCrawler crawler in this.Crawlers)
+                    {
+                        crawler.RebuildFromRoot(context, IndexingOptions.Default, CancellationToken.None);
+                    }
+
+                    context.Commit();
+                }
+
+                if (optimizeOnComplete && SolrContentSearchManager.SolrSettings.OptimizeOnRebuildEnabled())
+                {
+                    CrawlingLog.Log.Debug("[Index={0}] Optimizing collection [Collection: {1}]".FormatWith(this.Name, this.RebuildCollection));
+                    this.rebuildSolrOperations.Optimize();
+                }
+            }
+
+            this.SwitchAliasesCollections();
+            this.SetAliasesConfiguration();
+            this.PreserveAliasesCollections();
+
+            // Clear old collection.
+            // NOTE: clearing old collection after it's rebuild might be overengineering.
+            // However, if the indexes are big, it may save some disk space.
+            // Though disk space must be large enough to hold double of the current index size as both collections need to hold data while index is being rebuilt.
+            //if (resetIndex)
+            //{
+            //    this.Reset();
+            //}
+        }
+
+        /// <summary>
+        /// Sets main alias to the active collection and rebuild alias to the rebuild collection.
+        /// </summary>
+        private void SetAliasesConfiguration()
+        {
+            // Set primary alias (Core property value) to last preserved active collection.
+            this.SetAlias(this.Core, this.ActiveCollection);
+
+            // Set rebuild alias (RebuildCollection property value) to last preserved rebuild collection.
+            this.SetAlias(this.RebuildCore, this.RebuildCollection);
+        }
+
+        /// <summary>
+        /// Writes main alias and rebuild alias values to the index property store.
+        /// </summary>
+        private void PreserveAliasesCollections()
+        {
+            this.PropertyStore.Set(SolrContentSearchManager.RebuildCollection, this.RebuildCollection);
+            this.PropertyStore.Set(SolrContentSearchManager.ActiveCollection, this.ActiveCollection);
+        }
+
+        /// <summary>
+        /// Swaps aliases collections of main and rebuild aliases.
+        /// </summary>
+        private void SwitchAliasesCollections()
+        {
+            var activeCore = this.ActiveCollection.Clone().ToString();
+            this.ActiveCollection = this.RebuildCollection;
+            this.RebuildCollection = activeCore;
+            CrawlingLog.Log.Debug("[Index={0}] SwitchAliasesCollections: AliaseActiveCore switched to [Index={1}]  and RebuildCollection switched to [Index={2}].".FormatWith(this.Name, this.ActiveCollection, this.RebuildCollection));
+        }
+    }
+}
---
+++ src/Sitecore.Support.96016/ContentSearch/SolrProvider/SwitchOnRebuildSolrSearchIndex.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ContentSearch.SolrProvider
+{
+    using System.Threading;
+    using Sitecore.ContentSearch;
+    using Sitecore.ContentSearch.Diagnostics;
+    using Sitecore.ContentSearch.Maintenance;
+    using Sitecore.ContentSearch.Utilities;
+    using SolrNet;
+
+    public class SwitchOnRebuildSolrSearchIndex : Sitecore.ContentSearch.SolrProvider.SwitchOnRebuildSolrSearchIndex
+    {
+        public SwitchOnRebuildSolrSearchIndex(string name, string core, string rebuildcore, IIndexPropertyStore propertyStore) : base(name, core, rebuildcore, propertyStore)
+        {
+        }
+
+        protected override void PerformRebuild(bool resetIndex, bool optimizeOnComplete, IndexingOptions indexingOptions, CancellationToken cancellationToken)
+        {
+            if (!this.ShouldStartIndexing(indexingOptions))
+                return;
+
+
+            using (new RebuildIndexingTimer(this.PropertyStore))
+            {
+                if (resetIndex)
+                {
+                    this.Reset(this.tempSolrOperations, this.RebuildCore);
+                }
+
+                using (var context = this.CreateTemporaryCoreUpdateContext(this.tempSolrOperations))
+                {
+                    foreach (var crawler in this.Crawlers)
+                    {
+                        crawler.RebuildFromRoot(context, indexingOptions, cancellationToken);
+                    }
+
+                    context.Commit();
+                }
+
+                if (optimizeOnComplete)
+                {
+                    CrawlingLog.Log.Debug(string.Format("[Index={0}] Optimizing core [Core: {1}]", this.Name, this.RebuildCore));
+                    this.tempSolrOperations.Optimize();
+                }
+            }
+
+            this.SwapCores();
+        }
+        private void Reset(ISolrOperations<Dictionary<string, object>> operations, string coreName)
+        {
+            CrawlingLog.Log.Debug(string.Format("[Index={0}] Resetting index records [Core: {1}]", this.Name, coreName));
+
+            var query = new SolrQueryByField("_indexname", this.Name);
+            operations.Delete(query);
+            operations.Commit();
+        }
+
+        private void SwapCores()
+        {
+            CrawlingLog.Log.Debug(string.Format("[Index={0}] Swapping cores [{1} -> {2}]", this.Name, this.Core, this.RebuildCore));
+
+            var response = this.solrAdmin.Swap(this.Core, this.RebuildCore);
+
+            if (response.Status != 0)
+            {
+                CrawlingLog.Log.Error(string.Format("[Index={0}] Error swapping cores. [{1}]", this.Name, this.RebuildCore));
+            }
+        }
+    }
+}
